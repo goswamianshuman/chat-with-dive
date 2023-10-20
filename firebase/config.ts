69c4013f4a2636ec3a498f4a9261732842383b68
Anshuman Goswami <anshumangoswami07@gmail.com>
2023-10-20 18:28:04 +0530

    fix: config

--- firebase/config.ts
+++ firebase/config.ts
@@ -3,14 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  apiKey: "AIzaSyCHAv0CIrCH5VICDFddb-1bc7tnHzhBWZs",
+  authDomain: "chat-with-dive.firebaseapp.com",
   databaseURL: "https://chat-with-dive-default-rtdb.firebaseio.com",
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
+  projectId: "chat-with-dive",
+  storageBucket: "chat-with-dive.appspot.com",
+  messagingSenderId: "519973358951",
+  appId: "1:519973358951:web:5e6ed95d23fbdedc7c33c2",
 };
 
 // Initialize Firebase
