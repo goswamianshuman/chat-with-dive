3ca4d66ff8ee9132527826a4ee94763063b75add
Anshuman Goswami <anshumangoswami07@gmail.com>
2023-10-21 22:49:06 +0530

    Update Sidebar.tsx

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -8,12 +8,12 @@ type Props = {
 
 export default function Sidebar({ allUsers }: Props) {
   return (
-    <div className="border-r-[2px] border-[#2c2c2c2e] min-w-[300px] w-max max-w-[350px] overflow-y-scroll scrollbar-hide scroll-smooth transitionall ease-linear duration-100">
+    <div className="border-r-[2px] border-[#2c2c2c2e] min-w-[300px] w-max max-w-[350px] overflow-y-scroll scrollbar-hide scroll-smooth transition-all ease-linear duration-100">
       <div className="w-full h-max mt-3">
         <div className="px-5">
           <button
             onClick={() => {
-              alert("comming soon...");
+              alert("only users who have logged in there account's are listed here...");
             }}
             className="bg-[whitesmoke]/90 my-3 text-[#0b0c0e] px-4 py-3 text-xs uppercase tracking-wider font-medium rounded-md hover:bg-[whitesmoke]/90 active:bg-[whitesmoke] disabled:active:bg-[whitesmoke]/90 transition-all ease-linear duration-200 w-full disabled:cursor-not-allowed"
           >
